81c5cde8a01d3d86afbb8e58a9a2dae5b6adbcec
Sznapsollo <office@webproject.waw.pl>
2024-04-17 08:37:29 +0200

    initial commit

---
+++ controllers/actions.js
@@ -0,0 +1,35 @@
+const { response } = require("express");
+
+const customAPIAction = (filePath) => {
+  console.log('Hi am am your custom API action')
+  return {}
+}
+
+const handleAction = async (req, res = response) => {
+  console.log('handleAction', req?.body?.type)
+  // console.log(req.body)
+  
+  try {
+    let responseData = {status: 0, data: {}}
+    switch(req.body.type) {
+      case 'customAPIAction':
+        responseData.data = customAPIAction();
+        break
+      default:
+        responseData.status = -1;
+        responseData.data = 'Unrecognized action';
+        break;
+    }
+
+    res.json(responseData);
+  } catch (err) {
+    console.error(err);
+    responseData.status = -1;
+    responseData.data = err?.message ? err.message : "Operation failed";
+    res.json(responseData);
+  }
+};
+
+module.exports = {
+  handleAction,
+};
